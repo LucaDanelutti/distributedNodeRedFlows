50380095df1f93548fa2e97484c84fea8441585e
ian-ofgod <ian.didio@mail.polimi.it>
2021-09-16 12:32:50 +0200

    Code refactoring

--- custom-nodes/kafka-producer/kafka-producer.js
+++ custom-nodes/kafka-producer/kafka-producer.js
@@ -1,45 +1,39 @@
-const {logLevel} = require("kafkajs");
-const {Kafka} = require('kafkajs')
+const {logLevel, Kafka} = require("kafkajs");
+
+async function sendMessage(node, producer, topic, msg){
+    await producer.connect()
+    producer
+        .send({topic, messages: [{value: msg.payload}]})
+        .catch(e => node.warn(`[producer] ${e.message}`, e))
+    node.warn("SENT: "+msg.payload)
+}
 
 module.exports = function(RED) {
-
     function KafkaProducerNode(config) {
-        const broker = config.initialBroker
-        const id = config.clientId
+        RED.nodes.createNode(this, config);
+
         const node = this
-        const topic = config.topic
         const kafka = new Kafka({
-            logLevel: logLevel.DEBUG,
-            brokers: [`${broker}`],
-            clientId: id,
+            logLevel: logLevel.INFO,
+            brokers: [`${config.initialBroker}`],
+            clientId: config.clientId,
         }) //TODO: maybe make it global to reuse it
         const producer = kafka.producer()
-        const sendMessage = async (topic, msg) => {
-            await producer.connect()
-            producer
-                .send({topic, messages: [{value: msg.payload}]})
-                .then(console.log)
-                .catch(e => console.error(`[example/producer] ${e.message}`, e))
-        }
-
-        RED.nodes.createNode(this,config);
 
         node.on('input', function(msg) {
-            node.log("SENDING MESSAGE TO KAFKA TOPIC")
-            sendMessage(topic, msg)
-                .then(() => this.log("SEND SUCCESSFUL"))
-                .catch(err => this.error(err, msg))
+            node.warn("SENDING MESSAGE TO KAFKA TOPIC")
+            sendMessage(node, producer, config.topic, msg)
+                .then(() => node.warn("SEND SUCCESSFUL"))
+                .catch(err => node.warn(err, msg))
         })
 
-        node.on('close', function(removed, done) {
-            node.status({fill: 'red',shape: 'ring',text: 'closed'})
-            producer.close(false, () => {
+        node.on('close', function (removed, done) {
+            node.producer.close(false, () => {
                 node.log('closed')
             })
             done()
         })
+
     }
     RED.nodes.registerType("kafka-producer",KafkaProducerNode)
-
-
 }
